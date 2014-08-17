53973f764be3539d326e81045bd5b66eb5cc5d26
Patrick Kostjens <patrickkostjens@gmail.com>
2014-08-17 16:09:05 +0200

    Code cleanup

--- OutlookPrivacyPlugin/Recipient.cs
+++ OutlookPrivacyPlugin/Recipient.cs
@@ -18,11 +18,11 @@ namespace OutlookPrivacyPlugin
 		{
 			get
 			{
-				List<string> recipients = new List<string>();
+				var recipients = new List<string>();
 
-				for (int i = 0; i < KeyBox.Items.Count; i++)
+				for (var i = 0; i < KeyBox.Items.Count; i++)
 				{
-					GnuKey recipient = (GnuKey)KeyBox.Items[i];
+					var recipient = (GnuKey)KeyBox.Items[i];
 					if (KeyBox.GetItemChecked(i))
 						recipients.Add(recipient.Key);
 				}
@@ -40,7 +40,7 @@ namespace OutlookPrivacyPlugin
 		private void Passphrase_Load(object sender, EventArgs e)
 		{
 			// Did we locate all recipients keys?
-			bool unfoundKeys = true;
+			var unfoundKeys = true;
 
 			IList<GnuKey> keys = Encryption ? Globals.OutlookPrivacyPlugin.GetKeysForEncryption() : Globals.OutlookPrivacyPlugin.GetKeysForSigning();
 			if (keys.Count <= 0)
@@ -50,17 +50,18 @@ namespace OutlookPrivacyPlugin
 				return;
 			}
 
-			List<GnuKey> datasource = new List<GnuKey>();
-			int selectedCount = 0;
+			var datasource = new List<GnuKey>();
+			var selectedCount = 0;
 
 			// 1/ Collect selected keys and sort them.
-			foreach (GnuKey gnuKey in keys)
+			foreach (var gnuKey in keys)
 			{
-				if (null != _defaultKeys.Find(delegate(string key) { return gnuKey.Key.StartsWith(key, true, null); }))
+				if (_defaultKeys.Find(key => gnuKey.Key.StartsWith(key, true, null)) == null)
 				{
-					selectedCount++;
-					datasource.Add(gnuKey);
+					continue;
 				}
+				selectedCount++;
+				datasource.Add(gnuKey);
 			}
 
 			datasource.Sort(new GnuKeySorter());
@@ -72,18 +73,17 @@ namespace OutlookPrivacyPlugin
 			}
 
 			// 2/ Collect unselected keys and sort them.
-			List<GnuKey> datasource2 = new List<GnuKey>();
-			foreach (GnuKey gnuKey in keys)
+			var datasource2 = new List<GnuKey>();
+			foreach (var gnuKey in keys)
 			{
-				if (null == _defaultKeys.Find(delegate(string key) { return gnuKey.Key.StartsWith(key, true, null); }))
+				if (_defaultKeys.Find(key => gnuKey.Key.StartsWith(key, true, null)) == null)
 					datasource2.Add(gnuKey);
 			}
 
 			datasource2.Sort(new GnuKeySorter());
 
 			// Append unselected keys to datasource.
-			foreach (GnuKey gnuKey in datasource2)
-				datasource.Add(gnuKey);
+			datasource.AddRange(datasource2);
 
 			// Setup KeyBox
 			KeyBox.DataSource = datasource;
@@ -95,12 +95,12 @@ namespace OutlookPrivacyPlugin
 			Height = boxHeight + 90;
 
 			// Enlarge dialog to fit the longest key
-			using (Graphics g = CreateGraphics())
+			using (var g = CreateGraphics())
 			{
-				int maxSize = Width;
-				foreach (GnuKey key in datasource)
+				var maxSize = Width;
+				foreach (var key in datasource)
 				{
-					int textWidth = (int)g.MeasureString(key.KeyDisplay, KeyBox.Font).Width + 50;
+					var textWidth = (int)g.MeasureString(key.KeyDisplay, KeyBox.Font).Width + 50;
 					if (textWidth > maxSize)
 						maxSize = textWidth;
 				}
@@ -110,61 +110,14 @@ namespace OutlookPrivacyPlugin
 
 			// Note: Keybox sorted property MUST be False!
 			//       unless the custom sort strategy is voided!
-			for (int i = 0; i < selectedCount; i++)
+			for (var i = 0; i < selectedCount; i++)
 				KeyBox.SetItemChecked(i, true);
 
 			// If we found all the keys we don't need to show dialog
 			if (!unfoundKeys)
 			{
-				DialogResult = System.Windows.Forms.DialogResult.OK;
+				DialogResult = DialogResult.OK;
 				Close();
-			}
-		}
-
-		private void OldPassphrase_Load(object sender, EventArgs e)
-		{
-			IList<GnuKey> keys = Globals.OutlookPrivacyPlugin.GetKeysForEncryption();
-			KeyBox.DataSource = keys;
-			KeyBox.DisplayMember = "KeyDisplay";
-			KeyBox.ValueMember = "Key";
-
-			if (KeyBox.Items.Count <= 0)
-			{
-				// No keys available, no use in showing this dialog at all
-				Hide();
-				return;
-			}
-
-			int boxHeight = (keys.Count > 10) ? KeyBox.ItemHeight * 10 : KeyBox.ItemHeight * keys.Count;
-			KeyBox.Height = boxHeight + 5;
-			Height = boxHeight + 90;
-
-			// Enlarge dialog to fit the longest key
-			using (Graphics g = CreateGraphics())
-			{
-				int maxSize = Width;
-				foreach (GnuKey key in keys)
-				{
-					int textWidth = (int)g.MeasureString(key.KeyDisplay, KeyBox.Font).Width + 50;
-					if (textWidth > maxSize)
-						maxSize = textWidth;
-				}
-				Width = maxSize;
-				CenterToScreen();
-			}
-
-			for (int i = 0; i < KeyBox.Items.Count; i++)
-			{
-				GnuKey recipient = (GnuKey)KeyBox.Items[i];
-#if DISABLED
-        KeyBox.SetItemChecked(i, _defaultKeys.Contains(recipient.Key));
-#else
-				// Update to support CN from X.400 mail address format.
-				// Enable item if the associated key starts with one of the available _defaultKeys (hence a prefix match).
-				KeyBox.SetItemChecked(i,
-				  null != _defaultKeys.Find(delegate(string gnuKey) { return recipient.Key.StartsWith(gnuKey); }));
-#endif
-
 			}
 		}
 	}
