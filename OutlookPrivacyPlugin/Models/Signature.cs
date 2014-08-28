cafd3f467a3fd7468ce0843621fd3a440b6e7064
Patrick Kostjens <patrickkostjens@gmail.com>
2014-08-28 15:35:26 +0200

    Added forgotten signature class.

---
+++ OutlookPrivacyPlugin/Models/Signature.cs
@@ -0,0 +1,16 @@
+﻿namespace OutlookPrivacyPlugin.Models
+{
+	public class Signature
+	{
+		public bool Valid { get; set; }
+		public string UserId { get; set; }
+		public string KeyId { get; set; }
+
+		public override string ToString()
+		{
+			var message = Valid ? "Valid" : "Invalid";
+			message += " signature from " + UserId + " with key " + KeyId;
+			return message;
+		}
+	}
+}
